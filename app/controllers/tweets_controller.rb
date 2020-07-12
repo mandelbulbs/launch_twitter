f7ac7898977d11a474d9cf41f303a68eabb8bd4c
brayden <braydenmandavia@icloud.com>
2020-07-12 11:50:15 -0700

    added user authentication and required user to sign in to access 'New tweet' button

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,7 @@
 class TweetsController < ApplicationController
+	
+	before_action :authenticate_user!
+
 	def new
 		@tweet = Tweet.new
 		
@@ -6,17 +9,19 @@ class TweetsController < ApplicationController
 	end
 
 
-	def create
+	def create #def is an action
 		#@tweet = Tweet.new #params["tweet"]
 		#@tweet.content = params["tweet"]["content"]
 		#@tweet.save
 
 		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
 		if @tweet.save
 			flash[:success] = "Your tweet was succesfully published!"
 			redirect_to new_tweet_path
 		else
 			#once we're in here, there is a .errors
+			p @tweet.errors.inspect #for detecting user error (prevents non-users from posting)
 			render 'new'
 		end
 	end
