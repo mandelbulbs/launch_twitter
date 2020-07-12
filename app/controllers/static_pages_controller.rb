42ad667699cb0ba1913f6549a5228dfc08c6efa4
brayden <braydenmandavia@icloud.com>
2020-07-12 13:32:41 -0700

    created a redirect to the 'new tweet' feature for already signed in users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,14 @@
 class StaticPagesController < ApplicationController
 	def main_page
+		if user_signed_in?
+			if current_user.profile
+				redirect_to new_tweet_path
+			else
+				redirect_to new_profile_path
+			end
+		else
 		@mission_statement = "testing 1 testing 2 testing 3"
 	end
+end
 end
 
